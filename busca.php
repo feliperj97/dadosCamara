6e847cbc102b233130f6fc5d1f828572e86d43ff
Felipe Lamim <fclamim@gmail.com>
2019-01-15 15:08:01 -0200

    Adicionando mais dados

--- busca.php
+++ busca.php
@@ -15,7 +15,13 @@
 
     echo "Nome: " . $result->dados->nomeCivil . "<br>";
     echo "Estado: " . $result->dados->ultimoStatus->siglaUf . "<br>";
-    echo "Partido: " . $result->dados->ultimoStatus->siglaPartido;
+    echo "Partido: " . $result->dados->ultimoStatus->siglaPartido . "<br> <br>";
+    echo "---Gabinete--- <br>";
+    echo "Prédio: " . $result->dados->ultimoStatus->gabinete->predio. "<br>";
+    echo "Sala: " . $result->dados->ultimoStatus->gabinete->sala . "<br>";
+    echo "Andar: " . $result->dados->ultimoStatus->gabinete->andar . "<br>";
+    echo "Telefone: " . $result->dados->ultimoStatus->gabinete->telefone . "<br>";
+    echo "Email: " . $result->dados->ultimoStatus->gabinete->email . "<br>";
 
 
 
